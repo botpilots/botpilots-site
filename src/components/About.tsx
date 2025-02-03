e891b64279b4a552b7b732109a55e982606d5c9d
Oskar Huledal <55949372+hulsbo@users.noreply.github.com>
2025-02-03 22:52:32 +0100

    Enhance form layout (#2)
    * feat: enhance form layout for mobile and wide and short screens.
    
    * fix: change all lg to sm.
    
    * feat: gap-2 btw form title and form
    
    * feat: enhance About content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,9 +2,9 @@ import image from "../assets/meinpolo_softend.png"
 
 const About = (props: { className?: string }) => {
 	return (
-		<div className={`${props.className} flex gap-8 items-center flex-wrap lg:flex-nowrap justify-center`}>
+		<div className={`${props.className} flex gap-8 items-center flex-wrap sm:flex-nowrap justify-center`}>
 			{/* Image */}
-			<div className="py-2 lg:mr-4">
+			<div className="py-2 sm:mr-4">
 				<div className="rounded-full border-8 border-double border-slate-700 h-48 w-48 overflow-hidden my-2">
 					<img src={image} alt="Me in polo." className="scale-80 -translate-3" />
 				</div>
@@ -13,13 +13,10 @@ const About = (props: { className?: string }) => {
 			<div className="py-2 min-w-[250px] space-y-2">
 				<h2 className='text-4xl font-bold py-2 text-left'>About</h2>
 				<p className='text-lg text-left'>
-					Hello, I'm Oskar!
+					Hi, I'm Oskar. I founded BotPilots to combine my passion for coding with helping companies realize their web app visions.
 				</p>
 				<p className='text-lg text-left'>
-					I founded BotPilots to combine my passion for coding with helping companies realize their web app visions. I'm excited to offer you my expertise in implementing the web technologies of tomorrow.
-				</p>
-				<p className='text-lg text-left'>
-					Let's turn dreams into reality together!				</p>
+					I'm excited to offer you my expertise in implementing the web technologies of tomorrow. Let's turn dreams into reality together!				</p>
 				<div className="text-left py-2">
 					<p>Oskar Huledal - <i>Chief Bot Pilot & Founder</i></p>
 				</div>
