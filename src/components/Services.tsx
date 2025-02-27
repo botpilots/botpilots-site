45d3e7b58cd30aa0174e692357d03232b61c18c4
Oskar Huledal <55949372+hulsbo@users.noreply.github.com>
2025-02-27 23:29:42 +0100

    feat: delivering far and near (#14)

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -8,9 +8,12 @@ const Services = (props: { className?: string }) => {
 
 			<div className="flex-1 flex-col space-y-4 min-w-[250px]">
 				<h2 className='text-4xl font-bold text-left'>Our Services</h2>
-				<p id="p2" className='text-lg text-left'>
+				<p id="p2" className='text-left text-lg'>
 					From dynamic and eye-catching front-end designs to robust, reliable back-end systems,
 					BotPilots deliver performant and exciting web applications tailored to user needs and expectations.
+				</p>
+				<p id="p2" className='text-left text-lg'>
+					Delivering to clients near and far from Gothenburg, Sweden.
 				</p>
 			</div>
 		</div>
