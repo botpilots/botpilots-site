69e3c15787096f8825f4e06d805015fa9b33661f
hulsbo <oskar.huledal@simonsoft.se>
2025-08-14 19:32:49 +0200

    Adjust delays.

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,9 +1,9 @@
 import AnimatedHeader from './AnimatedHeader';
 
 const BASE_TEXT = "Looking for consultancy in  ";
-const WORDS = ["GenAI?", "XML?", "frontend?", "backend?"];
-const TYPE_SPEED = 80;
-const BACKSPACE_SPEED = 40;
+const WORDS = ["GenAI?", "XML?", "frontend?", "backend?", "AI translations?", "AI validations?"];
+const TYPE_SPEED = 100;
+const BACKSPACE_SPEED = 80;
 const WORD_PAUSE = 3000;
 
 const Services = (props: { className?: string }) => {
@@ -18,8 +18,8 @@ const Services = (props: { className?: string }) => {
 			<div className="flex-1 flex-col space-y-4 min-w-250px">
 				<AnimatedHeader
 					className='simple-fade-in'
-					style={{ animationDuration: '0.5s', animationDelay: '0s' }}
-					animationDelay={8000}
+					style={{ animationDuration: '0.8s', animationDelay: '0s' }}
+					animationDelay={10000}
 					baseText={BASE_TEXT}
 					words={WORDS}
 					typeSpeed={TYPE_SPEED}
@@ -29,26 +29,26 @@ const Services = (props: { className?: string }) => {
 				<h3 className="text-2xl font-light text-left">
 					<span
 						className="simple-fade-in"
-						style={{ animationDuration: '1s', animationDelay: '1s' }}
+						style={{ animationDuration: '1s', animationDelay: '2s' }}
 					>
 						With a rare to find expertise in both AI, XML and fullstack,{' '}
 					</span>
 					<span
 						className="simple-fade-in"
-						style={{ animationDuration: '1s', animationDelay: '1.7s' }}
+						style={{ animationDuration: '1s', animationDelay: '4s' }}
 					>
 						we offer innovative services and consultancy{' '}
 					</span>
 					<span
 						className="simple-fade-in"
-						style={{ animationDuration: '1s', animationDelay: '2.4s' }}
+						style={{ animationDuration: '1s', animationDelay: '6s' }}
 					>
 						to businesses working with technical documentation.
 					</span>
 				</h3>
 				<p 
 					className='text-left fade-in'
-					style={{ animationDuration: '1s', animationDelay: '6s' }}
+					style={{ animationDuration: '1s', animationDelay: '8s' }}
 				>
 					To learn more - scroll, swipe or press an arrow.
 				</p>
